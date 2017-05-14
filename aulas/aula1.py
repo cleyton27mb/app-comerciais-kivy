0cc9c29077fdce6d501bede1a18310c7515cce7a
sac <debhbrittes@gmail.com>
2017-05-14 07:52:31 -0400

    Initial commit

---
+++ aulas/aula1.py
@@ -0,0 +1,8 @@
+acao= int(input("Digite '1' para sim e '2' para não: "))
+if(acao==1):
+    print("Voce disse sim")
+else:
+    if(acao==2):
+     print("voce disse que não")
+    else:
+        print("o numero indicado não é 1 e nem 2")
---
+++ test/main.py
@@ -0,0 +1,8 @@
+num1 = float(input("Digite um numero:"))
+num2 = float(input("Digite outro numer"))
+
+divisao = num1/num2
+resto = num1 %num2
+
+print(num1, "dividido por ", num2, "é igual a: ", divisao)
+print("O resto da divisao entre", num1, "e", num2, "é", resto)
